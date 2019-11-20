5373d17aaba41debb93b9e4761d93f2f5c6f968e
Ivan Liang <ivanwenliang@gmail.com>
2019-11-20 11:08:59 -0800

    npm install

--- src/views/Main/Login/Login.js
+++ src/views/Main/Login/Login.js
@@ -1,67 +1,75 @@
-import React, { PropTypes as T } from 'react'
-import {Tabs, Col, Tab, ButtonToolbar, Button, FormGroup, FormControl, ControlLabel, Alert} from 'react-bootstrap'
-import AuthService from 'utils/AuthService'
-import styles from './styles.module.css'
+import React, { PropTypes as T } from "react";
+import {
+  Tabs,
+  Col,
+  Tab,
+  ButtonToolbar,
+  Button,
+  FormGroup,
+  FormControl,
+  ControlLabel,
+  Alert
+} from "react-bootstrap";
+import AuthService from "utils/AuthService";
+import styles from "./styles.module.css";
 
 export class Login extends React.Component {
   constructor(context) {
-    super()
+    super();
     this.state = {
-      user: '',
-      username: '',
-      email: '',
-      password: '',
-      loginError: '',
-      signupError: ''
-    }
+      user: "",
+      username: "",
+      email: "",
+      password: "",
+      loginError: "",
+      signupError: ""
+    };
   }
   static contextTypes = {
     router: T.object
-  }
+  };
 
   static propTypes = {
     location: T.object,
     auth: T.instanceOf(AuthService)
-  }
+  };
 
   onLoginSubmit(event) {
-    event.preventDefault()
-    const { user, password } = this.state
+    event.preventDefault();
+    const { user, password } = this.state;
     if (user && password) {
-      this.props.auth.login(user, password)
-        .then(result => {
-          if (!result.token) {
-            this.setState({loginError: result.message})
-            return
-          }
-          this.props.auth.finishAuthentication(result.token)
-          this.context.router.push('/profile')
-        })
+      this.props.auth.login(user, password).then(result => {
+        if (!result.token) {
+          this.setState({ loginError: result.message });
+          return;
+        }
+        this.props.auth.finishAuthentication(result.token);
+        this.context.router.history.push("/profile");
+      });
     }
   }
 
   onSignupSubmit(event) {
-    event.preventDefault()
-    const { username, email, password } = this.state
+    event.preventDefault();
+    const { username, email, password } = this.state;
     if (username && email && password) {
-      this.props.auth.signup(username, email, password)
-        .then(result => {
-          if (!result.token) {
-            this.setState({signupError: result.message})
-            return
-          }
-          this.props.auth.finishAuthentication(result.token)
-          this.context.router.push('/profile')
-        })
+      this.props.auth.signup(username, email, password).then(result => {
+        if (!result.token) {
+          this.setState({ signupError: result.message });
+          return;
+        }
+        this.props.auth.finishAuthentication(result.token);
+        this.context.router.push("/profile");
+      });
     }
   }
 
   handleChange(event) {
-    this.setState({[event.target.name]: event.target.value})
+    this.setState({ [event.target.name]: event.target.value });
   }
 
   render() {
-    const { auth } = this.props
+    const { auth } = this.props;
     return (
       <Col sm={6} smOffset={3}>
         <Tabs defaultActiveKey={1} id="loginTabs">
@@ -69,73 +77,77 @@ export class Login extends React.Component {
             <form onSubmit={this.onLoginSubmit.bind(this)}>
               <FormGroup controlId="user">
                 <ControlLabel>Username/Email</ControlLabel>
-                <FormControl 
+                <FormControl
                   type="text"
-                  placeholder="Enter your username or email" 
+                  placeholder="Enter your username or email"
                   name="user"
-                  value={this.state.user} 
+                  value={this.state.user}
                   onChange={this.handleChange.bind(this)}
                 />
               </FormGroup>
               <FormGroup controlId="user">
                 <ControlLabel>Password</ControlLabel>
-                <FormControl 
+                <FormControl
                   type="password"
-                  placeholder="Enter your password" 
-                  name="password" 
-                  value={this.state.password} 
+                  placeholder="Enter your password"
+                  name="password"
+                  value={this.state.password}
                   onChange={this.handleChange.bind(this)}
                 />
               </FormGroup>
 
-              <Button bsStyle="primary" type="submit">Submit</Button>
-              </form>
-              { this.state.loginError && 
-                <Alert bsStyle="danger">{this.state.loginError}</Alert>
-              }
+              <Button bsStyle="primary" type="submit">
+                Submit
+              </Button>
+            </form>
+            {this.state.loginError && (
+              <Alert bsStyle="danger">{this.state.loginError}</Alert>
+            )}
           </Tab>
           <Tab eventKey={2} title="Sign Up">
             <form onSubmit={this.onSignupSubmit.bind(this)}>
               <FormGroup controlId="user">
                 <ControlLabel>Username</ControlLabel>
-                <FormControl 
+                <FormControl
                   type="text"
-                  placeholder="Enter your username" 
-                  name="username" 
-                  value={this.state.username} 
+                  placeholder="Enter your username"
+                  name="username"
+                  value={this.state.username}
                   onChange={this.handleChange.bind(this)}
                 />
               </FormGroup>
               <FormGroup controlId="user">
                 <ControlLabel>Email</ControlLabel>
-                <FormControl 
+                <FormControl
                   type="text"
-                  placeholder="Enter your email" 
-                  name="email" 
-                  value={this.state.email} 
-                  onChange={this.handleChange.bind(this)}  
+                  placeholder="Enter your email"
+                  name="email"
+                  value={this.state.email}
+                  onChange={this.handleChange.bind(this)}
                 />
               </FormGroup>
               <FormGroup controlId="user">
                 <ControlLabel>Password</ControlLabel>
-                <FormControl 
+                <FormControl
                   type="password"
-                  placeholder="Enter your password" 
-                  name="password" 
-                  value={this.state.password} 
-                  onChange={this.handleChange.bind(this)}  
+                  placeholder="Enter your password"
+                  name="password"
+                  value={this.state.password}
+                  onChange={this.handleChange.bind(this)}
                 />
               </FormGroup>
-              <Button bsStyle="primary" type="submit">Submit</Button>
+              <Button bsStyle="primary" type="submit">
+                Submit
+              </Button>
             </form>
-            { this.state.signupError && 
+            {this.state.signupError && (
               <Alert bsStyle="danger">{this.state.signupError}</Alert>
-            }
+            )}
           </Tab>
         </Tabs>
       </Col>
-    )
+    );
   }
 }
 
-export default Login
+export default Login;
